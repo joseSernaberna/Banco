4012a503242e367e3fb40b650596b42e841d5e3f
joseSernaberna <jose.serna.berna@gmail.com>
2019-02-07 23:49:04 +0100

    Añadiendo lineas de codigo

--- Banco/src/banco/Banco.java
+++ Banco/src/banco/Banco.java
@@ -15,7 +15,10 @@ public class Banco {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+           System.out.println("Hola");
+           System.out.println("Primer paso");
+          
+
     }
     
 }
